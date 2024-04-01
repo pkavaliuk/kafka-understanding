6a479eb60f44ac6528725a2ca7bea98f0b015482
Pavel Kavaliuk <pavel.kavaliuk365@gmail.com>
2024-04-01 13:37:37 +0300

    first commit

---
+++ src/main/java/pavel/kavaliuk/kafkaunderstanding/KafkaUnderstandingApplication.java
@@ -0,0 +1,13 @@
+package pavel.kavaliuk.kafkaunderstanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaUnderstandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaUnderstandingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pavel/kavaliuk/kafkaunderstanding/KafkaUnderstandingApplicationTests.java
@@ -0,0 +1,13 @@
+package pavel.kavaliuk.kafkaunderstanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaUnderstandingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
